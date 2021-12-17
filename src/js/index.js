3ae28795845a0f36a8e3fb047567823c48613660
Seongha <11pi885@gmail.com>
2021-12-17 15:36:22 +0900

    메뉴추가기능 완료

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
 //TODO
 //메뉴 추가
 // 에스프레소 메뉴에 새로운 메뉴를 확인 버튼 또는 엔터키 입력으로 추가한다.
-//추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 // 총 메뉴 갯수를 count하여 상단에 보여준다.
 // 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // 사용자 입력값이 빈 값이라면 추가되지 않는다.
@@ -17,13 +17,15 @@ function App() {
         e.preventDefault();
     });
 
-    // 메뉴의 이름을 입력받기
-    $("#espresso-menu-name").addEventListener("keypress", (e) => {
-        if (e.key == "Enter") {
-            //console.log($("#espresso-menu-name").value);
-            const espressoMenuName = $("#espresso-menu-name").value;
-            const menuItemTemplate = (espressoMenuName) => {
-                return `
+    const addMenuName = () => {
+        if ($("#espresso-menu-name").value === "") {
+            alert("값을 입력해 주세요.");
+            return;
+        }
+        //console.log($("#espresso-menu-name").value);
+        const espressoMenuName = $("#espresso-menu-name").value;
+        const menuItemTemplate = (espressoMenuName) => {
+            return `
                 <li class="menu-list-item d-flex items-center py-2">
                     <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
                     <button
@@ -39,11 +41,27 @@ function App() {
                         삭제
                     </button>
                 </li>`;
-            };
-            $("#espresso-menu-list").insertAdjacentHTML(
-                'beforeend', menuItemTemplate(espressoMenuName)
-            );
+        };
+        $("#espresso-menu-list").insertAdjacentHTML(
+            'beforeend', menuItemTemplate(espressoMenuName)
+        );
+        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount}개`;
+
+        $("#espresso-menu-name").value = "";
+
+    }
+
+    $("#espresso-menu-submit-button").addEventListener("click", () => {
+        addMenuName();
+    });
+
+    // 메뉴의 이름을 입력받기
+    $("#espresso-menu-name").addEventListener("keypress", (e) => {
+        if (e.key !== "Enter") {
+            return;
         }
+        addMenuName();
     });
 }
 
